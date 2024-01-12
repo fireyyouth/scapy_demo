af1b9adaea66be65cca7a200f60448ac4adf1933
fireyyouth <fireyyouth@gmail.com>
2024-01-12 17:40:57 +0800

    Update syn_flood_test.py

--- syn_flood_test.py
+++ syn_flood_test.py
@@ -1,8 +1,11 @@
 from scapy.all import * # The One and Only Scapy
+import sys
+
+dport = int(sys.argv[1])
 
 conf.L3socket=L3RawSocket
 ip = IP(dst='localhost')
 
-syn = ip / TCP(sport=1500, dport=8081, flags="S", seq=1000)
+syn = ip / TCP(sport=1500, dport=dport, flags="S", seq=1000)
 
 syn_ack = sr1(syn)
